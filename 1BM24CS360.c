92b494fbf2d504eb278c18056709ae9199734975
aneeshggs <aneeshggs06@gmail.com>
2026-03-06 12:40:51 +0530

    Add files via upload

---
+++ 1BM24CS360.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int at[10], bt[10], wt[10], tat[10], ct[10];
+    float avg_wt = 0, avg_tat = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        printf("Enter Arrival Time and Burst Time for P%d: ", i+1);
+        scanf("%d %d", &at[i], &bt[i]);
+    }
+
+    ct[0] = at[0] + bt[0];
+    tat[0] = ct[0] - at[0];
+    wt[0] = tat[0] - bt[0];
+
+    for(i = 1; i < n; i++) {
+        ct[i] = ct[i-1] + bt[i];
+
+        if(ct[i-1] < at[i])
+            ct[i] = at[i] + bt[i];
+
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+    }
+
+    printf("\nP\tAT\tBT\tWT\tTAT\n");
+
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\n", i+1, at[i], bt[i], wt[i], tat[i]);
+        avg_wt += wt[i];
+        avg_tat += tat[i];
+    }
+
+    printf("\nAverage Waiting Time = %.2f", avg_wt/n);
+    printf("\nAverage Turnaround Time = %.2f", avg_tat/n);
+
+    return 0;
+}
